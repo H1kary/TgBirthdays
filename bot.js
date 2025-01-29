5b5682014973df507737f86fe87080413e0d65c9
H1kary <babylya03@yandex.ru>
2025-01-29 10:41:53 +0300

    Now Mistal AI uses special agent

--- bot.js
+++ bot.js
@@ -8,9 +8,10 @@ require("dotenv").config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const apiKey = process.env.MISTRAL_API_KEY;
+const agentId = process.env.AGENT_ID;
 const client = new Mistral({apiKey: apiKey});
 
-// Вебанутые - 1752588664
+// Вебанутые - -1001711466703  
 // H1kary - 1126975443
 
 bot.telegram.setMyCommands([
@@ -26,7 +27,7 @@ bot.start((ctx) => {
      `/birthdays - Список дней рождения` +
      `\n\n*Чтобы начать общение с ботом, просто напишите ему сообщение.*`
   );
-  ctx.telegram.sendMessage(1126975443, `${ctx.from.username} \n\n${ctx.message.text} \n\nИспользовал команду /start`);
+  ctx.telegram.sendMessage(1126975443, `${ctx.from.username} // ${ctx.message.chat.id} \n\n${ctx.message.text} \n\nИспользовал команду /start`);
 });
 
 
@@ -37,17 +38,23 @@ bot.command("birthdays", (ctx) => {
 
 // Планировщик для отправки ежедневных уведомлений
 schedule.scheduleJob('0 10 * * *', () => {
-   bot.telegram.sendMessage(1752588664, getBirthdaysMessage());
+   bot.telegram.sendMessage(-1001711466703  , getBirthdaysMessage());
    bot.telegram.sendMessage(1126975443, `Рассылка дней рождения отправлена`);
 });
 
 bot.on(message("text"), async (ctx) => {
-   const chatResponse = await client.chat.complete({
-      model: 'mistral-large-latest',
-      messages: [{role: 'user', content: `${ctx.message.text}`}],
-    });
-   ctx.replyWithMarkdown(chatResponse.choices[0].message.content);
-   ctx.telegram.sendMessage(1126975443, `${ctx.from.username} \n\n${ctx.message.text} \n\n${chatResponse.choices[0].message.content}`);
+   try {
+      const chatResponse = await client.agents.complete({
+         agentId: `${agentId}`,
+         messages: [{role: 'user', content: `${ctx.message.text}`}],
+      });
+      ctx.replyWithMarkdown(chatResponse.choices[0].message.content);
+
+      ctx.telegram.sendMessage(1126975443, `${ctx.from.username} // ${ctx.message.chat.id}  \n\n${ctx.message.text} \n\n${chatResponse.choices[0].message.content}`);
+   } catch (error) {
+      console.error(error);
+      ctx.reply('Произошла ошибка при обработке запроса. Пожалуйста, попробуйте позже.');
+   }
 });
 
 bot.launch();
